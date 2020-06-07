411ba806d4438bf3c67b7222db56095d0ed49dec
Damir <damir.varesanovic@gmail.com>
2020-06-07 14:56:14 +0200

    Create project

---
+++ src/test/java/com/outfit/fun7/Fun7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outfit.fun7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fun7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
