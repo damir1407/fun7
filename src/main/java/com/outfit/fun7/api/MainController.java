de927900d62c53fad84e65d61c7ffa78662270ed
Damir <damir.varesanovic@gmail.com>
2020-06-10 21:59:01 +0200

    Refactor MainController, add tests

--- src/main/java/com/outfit/fun7/api/MainController.java
+++ src/main/java/com/outfit/fun7/api/MainController.java
@@ -4,6 +4,9 @@ import com.outfit.fun7.model.Features;
 import com.outfit.fun7.model.User;
 import com.outfit.fun7.service.FeaturesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,17 +21,16 @@ public class MainController {
         this.featuresService = featuresService;
     }
 
-    @GetMapping(path="/features")
-    @ResponseBody
-    public String getFeatures(@RequestParam("userId") Long id,
+    @GetMapping(path="/features", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getFeatures(@RequestParam("userId") Long id,
                             @RequestParam("timezone") String timezone,
                             @RequestParam("cc") String cc) {
         Features feature = this.featuresService.getFeatures(id, timezone, cc);
         if(feature != null) {
-            return feature.toString();
+            return new ResponseEntity<String>(feature.toString(), HttpStatus.OK);
         }
         else {
-            return "[]";
+            return ResponseEntity.notFound().build();
         }
 
     }
--- src/test/java/com/outfit/fun7/Fun7ApplicationTests.java
+++ src/test/java/com/outfit/fun7/Fun7ApplicationTests.java
@@ -1,13 +1,95 @@
 package com.outfit.fun7;
 
+import com.outfit.fun7.api.MainController;
+import com.outfit.fun7.model.User;
+import com.outfit.fun7.repository.UserRepository;
+import com.outfit.fun7.service.FeaturesService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.hamcrest.core.Is;
+import static org.hamcrest.Matchers.*;
 
-@SpringBootTest
+@AutoConfigureMockMvc
+@SpringBootTest()
 class Fun7ApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void userWithHighUsageAndFromUS_hasMultiplayerEnabled() throws Exception {
+		String user_id = "5634161670881280";
+		String timezone = "America/New_York";
+		String countryCode = "US";
+
+		mvc.perform(MockMvcRequestBuilders.get("/api/v1/features?userId=" + user_id + "&timezone=" + timezone + "&cc=" + countryCode)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("multiplayer", Is.is("enabled")));
+	}
+
+	@Test
+	void userWithHighUsageAndFromSI_hasMultiplayerDisabled() throws Exception {
+		String user_id = "5634161670881280";
+		String timezone = "Europe/Ljubljana";
+		String countryCode = "SI";
+
+		mvc.perform(MockMvcRequestBuilders.get("/api/v1/features?userId=" + user_id + "&timezone=" + timezone + "&cc=" + countryCode)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("multiplayer", Is.is("disabled")));
+	}
+
+	@Test
+	void userWithLowUsageAndFromUS_hasMultiplayerDisabled() throws Exception {
+		String user_id = "5644004762845184";
+		String timezone = "America/New_York";
+		String countryCode = "US";
+
+		mvc.perform(MockMvcRequestBuilders.get("/api/v1/features?userId=" + user_id + "&timezone=" + timezone + "&cc=" + countryCode)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("multiplayer", Is.is("disabled")));
+	}
+
+	@Test
+	void userFromSI_hasCustomerSupportDisabled_afterWorkHours() throws Exception {
+		String user_id = "5634161670881280";
+		String timezone = "Europe/Ljubljana";
+		String countryCode = "SI";
+
+		mvc.perform(MockMvcRequestBuilders.get("/api/v1/features?userId=" + user_id + "&timezone=" + timezone + "&cc=" + countryCode)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("user-support", Is.is("disabled")));
+	}
+
+	@Test
+	void userFromSI_hasCustomerSupportEnabled_inWorkHours() throws Exception {
+		String user_id = "5634161670881280";
+		String timezone = "Europe/Ljubljana";
+		String countryCode = "SI";
+
+		mvc.perform(MockMvcRequestBuilders.get("/api/v1/features?userId=" + user_id + "&timezone=" + timezone + "&cc=" + countryCode)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("user-support", Is.is("enabled")));
 	}
 
 }
